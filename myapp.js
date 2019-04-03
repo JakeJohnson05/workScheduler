7b582300b2720b4470b2f76ce8eb1f859191c906
Jake Johnson <jakejohnson801@outlook.com>
2019-04-03 16:01:04 -0600

    updated header and footer

--- myapp.js
+++ myapp.js
@@ -1,13 +1,8 @@
 const express = require(`express`);
 const APP = express();
-const FAVICON = require(`serve-favicon`);
 const PORT = process.env.PORT || 5000;
 
 APP.use(express.static(`appExpressScheduler`));
-
-APP.use(FAVICON(`${ __dirname }/appExpressScheduler/favicon.ico`));
-APP.use(FAVICON(`${ __dirname }/appExpressScheduler/favicon_2.ico`));
-APP.use(FAVICON(`${ __dirname }/appExpressScheduler/favicon_3.ico`));
 
 APP.get(`/`, (req, res) => res.sendFile(`startWorkScheduler.html`, {root: __dirname+`/appExpressScheduler`}));
 
