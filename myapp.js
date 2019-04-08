9ec25502831136cfa2171aa101bfd41a18dfce13
Jake Johnson <jakejohnson801@outlook.com>
2019-04-08 08:15:05 -0600

    myapp.js dir changes

--- myapp.js
+++ myapp.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 5000;
 
 APP.use(express.static(`appExpressScheduler`));
 
-APP.get(`/`, (req, res) => res.sendFile(`startWorkScheduler.html`, {root: `%{ __dirname }/appExpressScheduler`}));
+APP.get(`/`, (req, res) => res.sendFile(`startWorkScheduler.html`, {root: __dirname +'/appExpressScheduler'}));
 
 APP.listen(PORT, () => console.log(`expressScheduler active on port: ${ PORT }`));
